fa91ec5a013c78caaba770d3bc4cda92234202a6
Mathieu7483 <11436@holbertonstudents.com>
2025-06-26 13:40:41 +0000

    Write a function that compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,10 +7,7 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-if (*s1 == *s2)
-return (0);
-else if (*s1 > *s2)
-return (*s1 - *s2);
-else
+while (*s1 == *s2 && *s1 != '\0')
+++s1, ++s2;
 return (*s1 - *s2);
 }
