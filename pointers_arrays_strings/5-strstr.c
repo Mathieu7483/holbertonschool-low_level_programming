6dd19ccaa833208eed6c09ffee9925fa1320f726
Mathieu7483 <11436@holbertonstudents.com>
2025-06-30 15:25:49 +0200

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_strstr - locates a substring
+*@haystack: string
+*@needle: substring
+*Return: adress of Needle or NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != 0; j++)
+{
+if (haystack[i] == needle[j])
+return (needle);
+}
+}
+return ('\0');
+}
