66a8d295bacbb21f300e1e71004f5106698627e7
Mathieu7483 <11436@holbertonstudents.com>
2025-06-30 16:25:52 +0200

    print chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,12 +6,12 @@
 void print_chessboard(char (*a)[8])
 {
 int row, col;
-for(row=0;row<8;row++)
-{ 
-for(col=0;col<8;col++) 
+for (row = 0; row < 8; row++)
 {
-_putchar(a[row][col]); 
+for (col = 0; col < 8; col++)
+{
+_putchar(a[row][col]);
 }
-_putchar('\n'); 
-}  
+_putchar('\n');
+}
 }
