8fb730329f4f9acdfecf611d7e6e6bb96cd8938d
Mathieu7483 <11436@holbertonstudents.com>
2025-07-02 09:26:54 +0200

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - check the code
+*@argc: argument counter
+*@argv: argument array value
+*Return: always 0 access
+*/
+int main(int argc, char *argv[])
+{
+(void) argc;  
+printf("%s\n" , argv[0]);
+return (0);
+}
