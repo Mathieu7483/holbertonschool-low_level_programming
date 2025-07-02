6c023fd33523359e5c067ded0c23a9f6a876a4ae
Mathieu7483 <11436@holbertonstudents.com>
2025-07-02 13:04:00 +0200

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -8,13 +8,17 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i, j;
+if (needle[0] == '\0')
+return (haystack);
 for (i = 0; haystack[i] != '\0'; i++)
 {
 for (j = 0; needle[j] != '\0'; j++)
 {
-if (haystack[i] == needle[j])
-return (haystack);
+if (haystack[i + j] != needle[j])
+break;
 }
+if (needle[j] == '\0')
+return (haystack + i);
 }
 return ('\0');
 }
