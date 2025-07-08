b90df7ac5245192c62dc4d2155fdc39134b4d78a
Mathieu7483 <11436@holbertonstudents.com>
2025-07-08 09:17:24 +0200

    malloc checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-*
-*
-*
+*malloc_checked - check the malloc function
+*@b: unsigned int
 */
-
 void *malloc_checked(unsigned int b)
+{
+void *mallocfunction = malloc (b);
+if (mallocfunction == NULL)
+exit(98);
+return (mallocfunction);
+}
