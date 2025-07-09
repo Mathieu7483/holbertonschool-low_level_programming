ec9937b8454fbec7f14b44c40e9a1bf0730fbee2
Mathieu7483 <11436@holbertonstudents.com>
2025-07-09 09:18:22 +0200

    init dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,20 +2,20 @@
 #include "dog.h"
 /**
 *print_dog - print the informations
-*
+*@d: element of information (name, age, owner)
 */
 void print_dog(struct dog *d)
 if (d == NULL)
 printf("");
 if (d->d.name == NULL)
-printf("d.name %s\n" name:(nil))
-else 
+printf("d.name %s\n" name : (nil))
+else
 printf("d.name %s\n" d->d.name)
 if (d->d.age == NULL)
-printf("d.age %.6f\n" age:(nil))
-else 
+printf("d.age %.6f\n" age : (nil))
+else
 printf("d.age %s\n" d->d.age)
 if (d->d.owner == NULL)
-printf("d.owner %s\n" owner:(nil))
-else 
+printf("d.owner %s\n" owner : (nil))
+else
 printf("d.owner %s\n" d->d.owner)
