38f228822676586780b9ad6d279256da0574c4c3
Mathieu7483 <11436@holbertonstudents.com>
2025-07-15 09:22:16 +0200

    exercice 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,19 +1,26 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stddef.h>
+#include <string.h>
 /**
 *get_op_func - 
 *@s: string S
-*Return: 
+*Return: comparison 
 */
+int (*get_op_func(char *s))(int, int)
+{
 op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
+{ "+", op_add },
+{ "-", op_sub },
+{ "*", op_mul },
+{ "/", op_div },
+{ "%", op_mod },
+{ NULL, NULL }
 };
 int i;
-int (*get_op_func(char *s))(int, int);
-if (s != op_add ||s != op_div ||s != op_mod ||s!=op_mul || s!= op_sub)
-return(NULL);
+for (i = 0; ops[i].op != NULL; i++)
+{
+if (*(ops[i].op) == *s && s[1] == '\0')
+return (ops[i].f);
+}
+return (NULL);
+}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,3 +1,31 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdlib.h>
-#include <stddef.h>
+#include <stdio.h>
+/**
+* main - main
+* @argc: arguments counter
+* @argv: arguments vector
+*
+* Return: operations
+*/
+int main(int argc, char *argv[])
+{
+int a, b, result;
+int (*oprt)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+oprt = get_op_func(argv[2]);
+if (oprt == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+result = oprt(a, b);
+printf("%d\n", result);
+return(result);
+}
