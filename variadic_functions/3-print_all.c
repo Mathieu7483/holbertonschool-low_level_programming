92b1e1e3c1523d59e93da7446e9858cf7e53b1b2
Mathieu7483 <11436@holbertonstudents.com>
2025-07-18 16:19:15 +0200

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,55 +2,46 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
-*print_all - print all the arguments
-*@format: format of the arguments
+* print_all - Prints all arguments based on the format string.
+* @format: A list of types of arguments passed to the function.
 */
 void print_all(const char * const format, ...)
 {
 va_list allargs;
 unsigned int i = 0;
 char *separator = "";
-char char_val;
-int int_val;
-float float_val;
 char *str_val;
 va_start(allargs, format);
 if (format == NULL)
 {
 printf("\n");
 va_end(allargs);
-return; }
+return;
+}
 while (format[i] != '\0')
 {
+if (format[i] == 'c' || format[i] == 'i'
+|| format[i] == 'f' || format[i] == 's')
+{
+printf("%s", separator);
+separator = ", ";
+}
 switch (format[i])
 {
 case 'c':
-printf("%s", separator);
-char_val = va_arg(allargs, int);
-printf("%c", char_val);
-separator = ", ";
+printf("%c", va_arg(allargs, int));
 break;
 case 'i':
-printf("%s", separator);
-int_val = va_arg(allargs, int);
-printf("%d", int_val);
-separator = ", ";
+printf("%d", va_arg(allargs, int));
 break;
 case 'f':
-printf("%s", separator);
-float_val = va_arg(allargs, double);
-printf("%f", float_val);
-separator = ", ";
+printf("%f", va_arg(allargs, double));
 break;
 case 's':
-printf("%s", separator);
 str_val = va_arg(allargs, char *);
 if (str_val == NULL)
-{str_val = "(nil)"; }
+str_val = "(nil)";
 printf("%s", str_val);
-separator = ", ";
-break;
-default:
 break; }
 i++;
 }
