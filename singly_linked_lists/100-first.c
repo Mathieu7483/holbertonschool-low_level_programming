d620df3b7afab641cdc454fb9df3644c0892c95e
Mathieu7483 <11436@holbertonstudents.com>
2025-07-28 14:58:24 +0200

    Add Function_before_main to execute code before main and create 100-main.c for testing

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+*Function_before_main - writing a function before main
+*
+*/
+void Function_before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -27,5 +27,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-
+void Function_before_main(void) __attribute__ ((constructor));
 #endif /* LISTS_H*/
