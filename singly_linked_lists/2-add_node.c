a27f520a2ca4e107e1fb81ffc2437fd841093cbc
Mathieu7483 <11436@holbertonstudents.com>
2025-07-28 14:32:22 +0200

    Refactor add_node function to correctly calculate string length and handle memory allocation

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,21 +9,23 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *dup_str;
+	unsigned int len = 0;
 
+	dup_str = strdup(str);
+
+if (dup_str == NULL)
+	return (NULL);
+while (str[len])
+len++;
 	new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 	{
-	return (NULL);
-	}
-	dup_str = strdup(dup_str);
-if (dup_str == NULL)
-	{
-	free(new_node);
+	free(dup_str);
 	return (NULL);
 	}
 
 	new_node->str = dup_str;
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
