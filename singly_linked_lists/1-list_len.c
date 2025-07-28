cbce8cf6f8f45d66ed37e3495e5e1fea71401f21
Mathieu7483 <11436@holbertonstudents.com>
2025-07-28 11:12:00 +0200

    Implement list_len function to count elements in a linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*list_len - lenght of arguments of a list
+*@h: Head
+*Return: Number of arguments
+*/
+size_t list_len(const list_t *h)
+
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+
+return (count);
+}
