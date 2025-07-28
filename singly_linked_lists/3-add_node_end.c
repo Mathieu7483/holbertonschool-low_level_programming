1e4db8d58cd6d36e294dec2015e204def1e097d4
Mathieu7483 <11436@holbertonstudents.com>
2025-07-28 13:25:12 +0200

    Add add_node_end function and main test file for linked list

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+*add_node_end - adding a new node at the end (tail)
+*@head: Double pointer to the head
+*@str: pointer to a string
+*Return: the address of the new element, or NULL if it failed
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temporary;
+	char *duplicate_str;
+
+
+	new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+	{
+	return (NULL);
+	}
+	duplicate_str = strdup(str);
+if (duplicate_str == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
+
+	new_node->str = duplicate_str;
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+else
+{
+	temporary = *head;
+	while (temporary->next != NULL)
+	{
+		temporary = temporary->next;
+	}
+		temporary->next = new_node;
+}
+return (new_node);
+}
