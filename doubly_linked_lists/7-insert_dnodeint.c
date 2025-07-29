199915d32c7cf20fbbcfb4723247f7cea69cf75d
Mathieu7483 <11436@holbertonstudents.com>
2025-07-29 15:39:41 +0200

    fixing betty 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,47 +11,41 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new_node;
 unsigned int i = 0;
-    
-if (h == NULL)
-    return (NULL);
 
+if (h == NULL)
+	return (NULL);
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
-    return (NULL);
+	return (NULL);
 new_node->n = n;
 if (idx == 0)
 {
-    new_node->prev = NULL;
-    new_node->next = *h;
-    if (*h != NULL)
-    {
-        (*h)->prev = new_node;
-    }
-    *h = new_node;
-    return (new_node);
+	new_node->prev = NULL;
+	new_node->next = *h;
+	if (*h != NULL)
+	{
+		(*h)->prev = new_node;
+	}
+	*h = new_node;
+	return (new_node);
 }
-
 temp = *h;
 while (temp != NULL && i < idx - 1)
 {
-    temp = temp->next;
-    i++;
+	temp = temp->next;
+	i++;
 }
 if (temp == NULL)
 {
-    free(new_node);
-     return (NULL);
+	free(new_node);
+	return (NULL);
 }
-
 new_node->next = temp->next;
 new_node->prev = temp;
-
 if (temp->next != NULL)
 {
-    temp->next->prev = new_node;
-}
+	temp->next->prev = new_node;
 
 temp->next = new_node;
-
 return (new_node);
 }
