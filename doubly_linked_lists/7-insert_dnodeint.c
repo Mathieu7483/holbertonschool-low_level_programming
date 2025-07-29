48f00a22aaae786177891e69facf9e7f6304f79b
Mathieu7483 <11436@holbertonstudents.com>
2025-07-29 15:07:11 +0200

    create J for testing, add 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+*insert_dnodeint_at_index - function that inserts
+*a new node at a given position.
+*@h: double pointer of Head
+*@idx: index of the list where the new node should be added. Index starts at 0
+*@n: Integer
+*Return: the address of the new node, or NULL if it failed
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *new_node, *temp = *h;
+unsigned int i = 0;
+
+if (h == NULL)
+return (NULL);
+
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+
+temp = *h;
+while (temp && i < idx - 1)
+{
+	temp = temp->next;
+	i++;
+}
+new_node->next = temp->next;
+new_node->prev = temp;
+
+if (temp->next)
+	temp->next->prev = new_node;
+
+temp->next = new_node;
+return (new_node);
+}
