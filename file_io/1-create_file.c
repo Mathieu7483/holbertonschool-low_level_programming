a9d96b0a14d63b6120a81ad7a6abc65d679f8e53
Mathieu7483 <11436@holbertonstudents.com>
2025-07-30 13:52:46 +0200

    fixing code for 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 
-fileD = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
+fileD = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fileD == -1)
 	{
 		return (-1);
