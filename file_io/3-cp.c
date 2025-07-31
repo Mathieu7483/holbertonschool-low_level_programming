d1ceb6f9578f4fe4bfb814721d4aa6e43037c5cc
Mathieu7483 <11436@holbertonstudents.com>
2025-07-31 14:28:46 +0200

    adding _strcmp to code and main.h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+*_strcmp - compare the string
+*@s1: source tab
+*@s2: dest tab
+*Return: value of comp
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 == *s2 && *s1 != '\0')
+++s1, ++s2;
+return (*s1 - *s2);
+}
+
+/**
 * copy_file - program that copies the content of a file to another file.
 * @ac: arguments count
 * @av: array of arguments.
@@ -14,6 +27,10 @@ int copy_file(int ac, char **av)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
+	if (_strcmp(av[1], av[2]) == 0)
+	{
+		return (0);
 	}
 	fileD_from = open(av[1], O_RDONLY);
 	if (fileD_from == -1)
