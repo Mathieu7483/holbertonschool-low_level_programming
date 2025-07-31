839fb70580963ab52957bcd8eed9f719913a6fc1
Mathieu7483 <11436@holbertonstudents.com>
2025-07-31 14:05:09 +0200

    Adding some libraries for 3-cp.c

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
 int _strlen(char *s);
 int _putchar(char c);
@@ -12,5 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+int main(int ac, char **av);
+int copy_file(int ac, char **av);
 
 #endif /* MAIN_H*/
