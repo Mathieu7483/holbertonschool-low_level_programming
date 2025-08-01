9e6bcb1630cc334848fddb14aa6fc6775c0ead51
Mathieu7483 <11436@holbertonstudents.com>
2025-08-01 11:05:03 +0200

    adding 5-hash_table_print.c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+*hash_table_print - function that prints a hash table
+*@ht: the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+}
