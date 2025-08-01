e80bf729573b884bbc94f7a370c5ec65daf6e44a
Mathieu7483 <11436@holbertonstudents.com>
2025-08-01 11:24:18 +0200

    update code 5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -5,6 +5,25 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i, count = 0;
+	hash_node_t *node;
+
 	if (ht == NULL)
 		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (count > 0)
+					printf(", ");
+				printf("'%s': '%s'", node->key);
+				node = node->next;
+			}
+		}
+	}
+	printf("}\n");
 }
